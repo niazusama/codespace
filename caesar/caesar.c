3f9a965ddc3bd6558fd8dba82cdc668771fe80a6
niazusama <131873257+niazusama@users.noreply.github.com>
2023-05-10 11:56:34 +0000

    Wed, May 10, 2023, 11:56 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
@@ -7,10 +8,19 @@ int main(int argc, string argv[])
     if (argc == 1)
     {
         printf("Please enter the key!\n");
+        return 1;
     }
     else if (argc > 2)
     {
         printf("Usage: %s key\n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        for (int i = 0, n = strlen(argv[1]); i < n; i++)
+        {
+            printf("%c\n", argv[1][i]);
+        }
     }
 
 }
