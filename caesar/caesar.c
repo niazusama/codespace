1975725d4b96873316120273ec5ed0a95474e105
niazusama <131873257+niazusama@users.noreply.github.com>
2023-05-10 11:50:30 +0000

    Wed, May 10, 2023, 11:50 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,5 +3,10 @@
 
 int main(int argc, string argv[])
 {
-    
+    // If no command line argument is provided
+    if (argc == 1)
+    {
+        printf("Please enter the key!\n")
+    }
+    else if (argc )
 }
