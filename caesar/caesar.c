5252d643544747c6454033404cde50131399bab1
niazusama <131873257+niazusama@users.noreply.github.com>
2023-05-10 11:59:23 +0000

    Wed, May 10, 2023, 11:59 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,13 +10,16 @@ int main(int argc, string argv[])
         printf("Please enter the key!\n");
         return 1;
     }
+    // if more than one command line argument is provided
     else if (argc > 2)
     {
         printf("Usage: %s key\n", argv[0]);
         return 1;
     }
+    // If only one command line argument is provided
     else
     {
+        // Finding if the key is Numerica
         for (int i = 0, n = strlen(argv[1]); i < n; i++)
         {
             printf("%c\n", argv[1][i]);
