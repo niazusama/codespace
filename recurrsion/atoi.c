eab789f35f2b75833a7bf7831250d6217cf474af
niazusama <131873257+niazusama@users.noreply.github.com>
2023-05-14 20:53:28 +0000

    Sun, May 14, 2023, 8:53 PM +00:00

--- recurrsion/atoi.c
+++ recurrsion/atoi.c
@@ -5,12 +5,24 @@
 int main(int argc, string argv[])
 {
     printf("%lu\n", strlen(argv[1]));
+
 }
 
 void atio(string word)
 {
+
     // Finding the length of string
     int n = strlen(word);
 
-    
+    if (n == 0)
+    {
+        return;
+    }
+    else
+    {
+        atio(word);
+    }
+
+    int val = word[n-1];
+    word[n-1] = '\0';
 }
